7c72a240ccad6e7fa15a93fa17fdbffc20d67bd7
KAldhalimi <karrarmajed@gmail.com>
2019-01-15 10:43:58 -0500

    inital commit

---
+++ PracticeTwo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("please enter a number ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.Write("please enter another number ");
+            int y = int.Parse(Console.ReadLine());
+
+            int z = x + y;
+
+            Console.Write("the sum of {0} and {1} equals {2}",x,y,z);
+            Console.ReadKey();
+        }
+    }
+}
